af137b56b99aa03cf863ece93354d1b0a0b136a5
Yeon Woo Park <ywoo1234@gmail.com>
2020-05-03 13:54:56 -0400

    Use tryLock function to acquire the lock and display error message if failed

--- src/com/ywoo/BankAccount.java
+++ src/com/ywoo/BankAccount.java
@@ -1,5 +1,6 @@
 package com.ywoo;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -27,21 +28,31 @@ class BankAccount {
 
     // Using synchronized object(this)
     public void deposit(double amount) {
-        lock.lock();
         try {
-            balance += amount;
-        } finally {
-            lock.unlock();
-        }
+            if(lock.tryLock(1000, TimeUnit.MILLISECONDS)) {
+                try {
+                    balance += amount;
+                } finally {
+                    lock.unlock();
+                }
+            } else {
+                System.out.println("Could not get the lock");
+            }
+        } catch(InterruptedException e) {}
     }
 
     public void withdraw(double amount) {
-        lock.lock();
         try {
-            balance -= amount;
-        } finally {
-            lock.unlock();
-        }
+            if(lock.tryLock(1000, TimeUnit.MILLISECONDS)) {
+                try {
+                    balance -= amount;
+                } finally {
+                    lock.unlock();
+                }
+            } else {
+                System.out.println("Could not get the lock");
+            }
+        } catch(InterruptedException e) {}
     }
 
     // No need to add synchronized keyword here
